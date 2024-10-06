9202aaf4ac5877e6f9b6b7535886a5f7ec69e7d5
GongjoongDoduk <noreply@github.com>
2024-10-06 21:15:33 +0900

    Fix registry sync issue

--- src/main/java/org/gjdd/batoru/component/BatoruDataComponentTypes.java
+++ src/main/java/org/gjdd/batoru/component/BatoruDataComponentTypes.java
@@ -1,5 +1,6 @@
 package org.gjdd.batoru.component;
 
+import eu.pb4.polymer.core.api.other.PolymerComponent;
 import net.minecraft.component.ComponentType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,5 +15,6 @@ public final class BatoruDataComponentTypes {
 
     public static void register() {
         Registry.register(Registries.DATA_COMPONENT_TYPE, "batoru:usable_job", USABLE_JOB);
+        PolymerComponent.registerDataComponent(USABLE_JOB);
     }
 }
