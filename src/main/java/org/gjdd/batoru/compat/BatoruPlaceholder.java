09d5e43e9854e8099d331297f169ab5f19359c6b
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2024-10-12 19:58:35 +0900

    쿨다운 초 표현 추가

--- src/main/java/org/gjdd/batoru/compat/BatoruPlaceholder.java
+++ src/main/java/org/gjdd/batoru/compat/BatoruPlaceholder.java
@@ -27,7 +27,7 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_1) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_1) == null) return PlaceholderResult.value("None");
 
             return PlaceholderResult.value(skillmap.get(SkillSlot.NORMAL_1).value().getName());
         });
@@ -40,7 +40,7 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_2) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_2) == null) return PlaceholderResult.value("None");
 
             return PlaceholderResult.value(skillmap.get(SkillSlot.NORMAL_2).value().getName());
         });
@@ -53,7 +53,7 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_3) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_3) == null) return PlaceholderResult.value("None");
 
             return PlaceholderResult.value(skillmap.get(SkillSlot.NORMAL_3).value().getName());
         });
@@ -66,11 +66,12 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.ULTIMATE) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.ULTIMATE) == null) return PlaceholderResult.value("None");
 
             return PlaceholderResult.value(skillmap.get(SkillSlot.ULTIMATE).value().getName());
         });
 
+        //쿨다운 초 표현
         Placeholders.register(Identifier.of("batoru", "skill_normal_1_cooldown"), (ctx, arg) -> {
             if (!ctx.hasEntity()) return PlaceholderResult.invalid("No Entity!");
             if (!ctx.entity().isLiving()) return PlaceholderResult.invalid("Not LivingEntity!");
@@ -79,9 +80,9 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_1) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_1) == null) return PlaceholderResult.value("0");
 
-            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_1))));
+            return PlaceholderResult.value(String.valueOf(Math.ceil((double) e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_1))/20)));
         });
 
         Placeholders.register(Identifier.of("batoru", "skill_normal_2_cooldown"), (ctx, arg) -> {
@@ -92,9 +93,9 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_2) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_2) == null) return PlaceholderResult.value("0");
 
-            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_2))));
+            return PlaceholderResult.value(String.valueOf(Math.ceil((double) e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_2))/20)));
         });
 
         Placeholders.register(Identifier.of("batoru", "skill_normal_3_cooldown"), (ctx, arg) -> {
@@ -105,9 +106,9 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.NORMAL_3) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.NORMAL_3) == null) return PlaceholderResult.value("0");
 
-            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_3))));
+            return PlaceholderResult.value(String.valueOf(Math.ceil((double) e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_3))/20)));
         });
 
         Placeholders.register(Identifier.of("batoru", "skill_ultimate_cooldown"), (ctx, arg) -> {
@@ -118,7 +119,60 @@ public class BatoruPlaceholder {
             if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
 
             var skillmap = e.getJob().value().getSkillMap();
-            if (skillmap.get(SkillSlot.ULTIMATE) == null) return PlaceholderResult.invalid("No Skill!");
+            if (skillmap.get(SkillSlot.ULTIMATE) == null) return PlaceholderResult.value("0");
+
+            return PlaceholderResult.value(String.valueOf(Math.ceil((double) e.getSkillCooldown(skillmap.get(SkillSlot.ULTIMATE))/20)));
+        });
+
+        //쿨다운 틱 표현
+        Placeholders.register(Identifier.of("batoru", "skill_normal_1_cooldown_tick"), (ctx, arg) -> {
+            if (!ctx.hasEntity()) return PlaceholderResult.invalid("No Entity!");
+            if (!ctx.entity().isLiving()) return PlaceholderResult.invalid("Not LivingEntity!");
+
+            var e = (LivingEntity) ctx.entity();
+            if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
+
+            var skillmap = e.getJob().value().getSkillMap();
+            if (skillmap.get(SkillSlot.NORMAL_1) == null) return PlaceholderResult.value("0");
+
+            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_1))));
+        });
+
+        Placeholders.register(Identifier.of("batoru", "skill_normal_2_cooldown_tick"), (ctx, arg) -> {
+            if (!ctx.hasEntity()) return PlaceholderResult.invalid("No Entity!");
+            if (!ctx.entity().isLiving()) return PlaceholderResult.invalid("Not LivingEntity!");
+
+            var e = (LivingEntity) ctx.entity();
+            if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
+
+            var skillmap = e.getJob().value().getSkillMap();
+            if (skillmap.get(SkillSlot.NORMAL_2) == null) return PlaceholderResult.value("0");
+
+            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_2))));
+        });
+
+        Placeholders.register(Identifier.of("batoru", "skill_normal_3_cooldown_tick"), (ctx, arg) -> {
+            if (!ctx.hasEntity()) return PlaceholderResult.invalid("No Entity!");
+            if (!ctx.entity().isLiving()) return PlaceholderResult.invalid("Not LivingEntity!");
+
+            var e = (LivingEntity) ctx.entity();
+            if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
+
+            var skillmap = e.getJob().value().getSkillMap();
+            if (skillmap.get(SkillSlot.NORMAL_3) == null) return PlaceholderResult.value("0");
+
+            return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.NORMAL_3))));
+        });
+
+        Placeholders.register(Identifier.of("batoru", "skill_ultimate_cooldown_tick"), (ctx, arg) -> {
+            if (!ctx.hasEntity()) return PlaceholderResult.invalid("No Entity!");
+            if (!ctx.entity().isLiving()) return PlaceholderResult.invalid("Not LivingEntity!");
+
+            var e = (LivingEntity) ctx.entity();
+            if (e.getJob() == null) return PlaceholderResult.invalid("No Job!");
+
+            var skillmap = e.getJob().value().getSkillMap();
+            if (skillmap.get(SkillSlot.ULTIMATE) == null) return PlaceholderResult.value("0");
 
             return PlaceholderResult.value(String.valueOf(e.getSkillCooldown(skillmap.get(SkillSlot.ULTIMATE))));
         });
