970bf3fd0092a9c75cbb58343bd00c45524cfd89
GongjoongDoduk <noreply@github.com>
2024-10-13 22:41:19 +0900

    Update ChannelingBuilder

--- src/main/java/org/gjdd/batoru/channeling/builder/ChannelingBuilder.java
+++ src/main/java/org/gjdd/batoru/channeling/builder/ChannelingBuilder.java
@@ -4,12 +4,35 @@ import org.gjdd.batoru.channeling.Channeling;
 import org.gjdd.batoru.channeling.ChannelingContext;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * {@link Channeling}의 빌더 클래스입니다.
  */
 public final class ChannelingBuilder {
+    private Predicate<ChannelingContext> ignoreSilenced = context -> false;
     private Consumer<ChannelingContext> onTick = context -> {};
+
+    /**
+     * {@link Channeling#ignoreSilenced} 메서드를 주어진 람다로 설정합니다.
+     *
+     * @param ignoreSilenced Predicate 객체
+     * @return 자기 자신 객체
+     */
+    public ChannelingBuilder ignoreSilenced(Predicate<ChannelingContext> ignoreSilenced) {
+        this.ignoreSilenced = ignoreSilenced;
+        return this;
+    }
+
+    /**
+     * {@link Channeling#ignoreSilenced} 메서드가 항상 주어진 boolean 값을 반환하도록 설정합니다.
+     *
+     * @param ignoreSilenced boolean 값
+     * @return 자기 자신 객체
+     */
+    public ChannelingBuilder ignoreSilenced(boolean ignoreSilenced) {
+        return ignoreSilenced(context -> ignoreSilenced);
+    }
 
     /**
      * {@link Channeling#onTick} 메서드를 주어진 람다로 설정합니다.
@@ -28,6 +51,16 @@ public final class ChannelingBuilder {
      * @return Channeling 객체
      */
     public Channeling build() {
-        return context -> onTick.accept(context);
+        return new Channeling() {
+            @Override
+            public boolean ignoreSilenced(ChannelingContext context) {
+                return ignoreSilenced.test(context);
+            }
+
+            @Override
+            public void onTick(ChannelingContext context) {
+                onTick.accept(context);
+            }
+        };
     }
 }
