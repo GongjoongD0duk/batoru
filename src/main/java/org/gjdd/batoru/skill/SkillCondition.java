74ad503d91bf0ac201f7d8a75d2e487958f52f34
GongjoongDoduk <noreply@github.com>
2024-10-14 10:39:41 +0900

    Add 'and' and 'or' methods to SkillCondition

--- src/main/java/org/gjdd/batoru/skill/SkillCondition.java
+++ src/main/java/org/gjdd/batoru/skill/SkillCondition.java
@@ -53,4 +53,30 @@ public interface SkillCondition {
      * @return 스킬 사용 가능 여부
      */
     SkillActionResult canUse(SkillContext context);
+
+    /**
+     * 현재 스킬 조건과 주어진 스킬 조건을 모두 만족해야 하는 결합된 스킬 조건을 생성하여 반환합니다.
+     * 즉, 두 조건이 모두 성공해야 스킬을 사용할 수 있습니다.
+     *
+     * @param other 결합할 스킬 조건
+     * @return 결합된 {@link SkillCondition} 객체
+     */
+    default SkillCondition and(SkillCondition other) {
+        return context -> canUse(context) instanceof SkillActionResult.Failure failure ?
+                failure :
+                other.canUse(context);
+    }
+
+    /**
+     * 현재 스킬 조건과 주어진 스킬 조건 중 하나라도 만족하면 되는 결합된 스킬 조건을 생성하여 반환합니다.
+     * 즉, 두 조건 중 하나라도 성공하면 스킬을 사용할 수 있습니다.
+     *
+     * @param other 결합할 스킬 조건
+     * @return 결합된 {@link SkillCondition} 객체
+     */
+    default SkillCondition or(SkillCondition other) {
+        return context -> canUse(context) instanceof SkillActionResult.Success success ?
+                success :
+                other.canUse(context);
+    }
 }
