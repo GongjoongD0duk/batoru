5c6628c9c4b1133f8399f3a1afd586998152126d
GongjoongDoduk <noreply@github.com>
2024-10-15 23:02:12 +0900

    Add default skill conditions

--- src/main/java/org/gjdd/batoru/skill/SkillCondition.java
+++ src/main/java/org/gjdd/batoru/skill/SkillCondition.java
@@ -6,6 +6,75 @@ package org.gjdd.batoru.skill;
 public interface SkillCondition {
 
     /**
+     * 일반적인 상황에서 사용되는 스킬 조건을 반환합니다. 이 스킬 조건은 관전자, 사망, 쿨다운,
+     * 정신 집중, 침묵 상태를 확인합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition defaultCondition() {
+        return failIfSpectator()
+                .and(failIfDead())
+                .and(failIfCooldown())
+                .and(failIfChanneling())
+                .and(failIfSilenced());
+    }
+
+    /**
+     * 관전자 상태일 때 실패하는 스킬 조건을 반환합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition failIfSpectator() {
+        return context -> context.source().isSpectator() ?
+                SkillActionResult.unavailable() :
+                SkillActionResult.success();
+    }
+
+    /**
+     * 사망 상태일 때 실패하는 스킬 조건을 반환합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition failIfDead() {
+        return context -> context.source().isDead() ?
+                SkillActionResult.unavailable() :
+                SkillActionResult.success();
+    }
+
+    /**
+     * 쿨다운일 때 실패하는 스킬 조건을 반환합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition failIfCooldown() {
+        return context -> context.source().hasSkillCooldown(context.skill()) ?
+                SkillActionResult.cooldown() :
+                SkillActionResult.success();
+    }
+
+    /**
+     * 정신 집중 중일 때 실패하는 스킬 조건을 반환합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition failIfChanneling() {
+        return context -> context.source().isChanneling() ?
+                SkillActionResult.channeling() :
+                SkillActionResult.success();
+    }
+
+    /**
+     * 침묵 상태일 때 실패하는 스킬 조건을 반환합니다.
+     *
+     * @return 스킬 조건 객체
+     */
+    static SkillCondition failIfSilenced() {
+        return context -> context.source().hasSilencedStatusEffect() ?
+                SkillActionResult.silenced() :
+                SkillActionResult.success();
+    }
+
+    /**
      * 스킬을 사용할 수 있는지 여부를 반환합니다.
      *
      * @param context 스킬 컨텍스트
